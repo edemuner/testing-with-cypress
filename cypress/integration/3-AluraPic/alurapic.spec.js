67c47b246c83255ccf8dee64e7d3b71429b9a8e7
edemuner <eduardood15@hotmail.com>
2022-04-17 13:10:25 -0300

    made a test to check for valid e-mail typing and valid password's length

--- cypress/integration/3-AluraPic/alurapic.spec.js
+++ cypress/integration/3-AluraPic/alurapic.spec.js
@@ -14,4 +14,18 @@ describe('Login e registro de usuarios alura pic', () => {
         cy.contains('ap-vmessage', 'Password is required!').should('be.visible')
         
     })
+
+    it('verifica mensagem email invalido', () => {
+        cy.contains('a', 'Register now').click()
+        cy.contains('button', 'Register').click()
+        cy.get('input[formcontrolname="email"]').type('anything')   
+        cy.contains('ap-vmessage', 'Invalid e-mail').should('be.visible')
+    })
+
+    it('verifica mensagem senha com menos de 8 caracteres', () => {
+        cy.contains('a', 'Register now').click()
+        cy.contains('button', 'Register').click()
+        cy.get('input[formcontrolname="password"]').type('lesst8').blur()
+        cy.contains('ap-vmessage', 'Mininum length is 8').should('be.visible')
+    })
 })
